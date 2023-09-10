278fa77c7b99a9cfcf79175625a33246b8b57d4b
yuk6ra <yuki.imamura.all@gmail.com>
2023-09-10 17:08:18 +0900

    Add cancel loan

--- x/loan/keeper/msg_server_cancel_loan.go
+++ x/loan/keeper/msg_server_cancel_loan.go
@@ -1,17 +1,33 @@
 package keeper
 
 import (
-	"context"
+    "context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"loan/x/loan/types"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+    "loan/x/loan/types"
 )
 
 func (k msgServer) CancelLoan(goCtx context.Context, msg *types.MsgCancelLoan) (*types.MsgCancelLoanResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
-	return &types.MsgCancelLoanResponse{}, nil
+    ctx := sdk.UnwrapSDKContext(goCtx)
+    loan, found := k.GetLoan(ctx, msg.Id)
+    if !found {
+        return nil, sdkerrors.Wrapf(sdkerrors.ErrKeyNotFound, "key %d doesn't exist", msg.Id)
+    }
+    if loan.Borrower != msg.Creator {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Cannot cancel: not the borrower")
+    }
+    if loan.State != "requested" {
+        return nil, sdkerrors.Wrapf(types.ErrWrongLoanState, "%v", loan.State)
+    }
+    borrower, _ := sdk.AccAddressFromBech32(loan.Borrower)
+    collateral, _ := sdk.ParseCoinsNormalized(loan.Collateral)
+    err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, borrower, collateral)
+    if err != nil {
+        return nil, err
+    }
+    loan.State = "cancelled"
+    k.SetLoan(ctx, loan)
+    return &types.MsgCancelLoanResponse{}, nil
 }
