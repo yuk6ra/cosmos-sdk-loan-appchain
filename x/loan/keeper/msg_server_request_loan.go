acda66adde41c199a7c706a220be553b2c779e3c
yuk6ra <yuki.imamura.all@gmail.com>
2023-09-10 16:31:11 +0900

    Add keeper

--- x/loan/keeper/msg_server_request_loan.go
+++ x/loan/keeper/msg_server_request_loan.go
@@ -9,9 +9,34 @@ import (
 
 func (k msgServer) RequestLoan(goCtx context.Context, msg *types.MsgRequestLoan) (*types.MsgRequestLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	var loan = types.Loan{
+		Amount: msg.Amount,
+		Fee: msg.Fee,
+		Collateral: msg.Collateral,
+		Deadline: msg.Deadline,
+		State: "requested",
+		Borrower: msg.Creator,
+	}
+	
+	// msg.Creator フィールドから借り手のアドレスを取得する
+	borrorer, err := sdk.AccAddressFromBech32(msg.Creator)
 
-	// TODO: Handling the message
-	_ = ctx
+	if err != nil {
+		panic(err)
+	}
+	
+	collateral, err := sdk.ParseCoinsNormalized(msg.Collateral)
+
+	if err != nil {
+		panic(err)
+	}
+
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, borrorer, types.ModuleName, collateral)
+	if sdkError != nil {
+		return nil, sdkError
+	}
+
+	k.AppendLoan(ctx, loan)
 
 	return &types.MsgRequestLoanResponse{}, nil
 }
