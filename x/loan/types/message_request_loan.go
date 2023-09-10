2bce1810c0b87c33e666d7d9648553f4acb666ad
yuk6ra <yuki.imamura.all@gmail.com>
2023-09-10 16:42:19 +0900

    Add ValidateBasic

--- x/loan/types/message_request_loan.go
+++ x/loan/types/message_request_loan.go
@@ -45,5 +45,32 @@ func (msg *MsgRequestLoan) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+	
+	amount, _ := sdk.ParseCoinsNormalized(msg.Amount)
+    if !amount.IsValid() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is not a valid Coins object")
+    }
+    if amount.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is empty")
+    }
+    fee, _ := sdk.ParseCoinsNormalized(msg.Fee)
+    if !fee.IsValid() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fee is not a valid Coins object")
+    }
+    deadline, err := strconv.ParseInt(msg.Deadline, 10, 64)
+    if err != nil {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "deadline is not an integer")
+    }
+    if deadline <= 0 {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "deadline should be a positive integer")
+    }
+    collateral, _ := sdk.ParseCoinsNormalized(msg.Collateral)
+    if !collateral.IsValid() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is not a valid Coins object")
+    }
+    if collateral.Empty() {
+        return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is empty")
+    }
+
 	return nil
 }
